1aa52c05d2c7c9c80ed482ab787128ce50e63b7f
majdmari <majdabueliass@gmail.com>
2024-07-16 18:10:39 +0300

    Creates edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,53 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                "Build your Career with Majd Mari",
-                style: TextStyle(
-                    color: Colors.black.withOpacity(.4), fontSize: 18),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "May21,2022",
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 16,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  "Build your Career with Majd Mari",
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(.4), fontSize: 18),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                ),
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                "May21,2022",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.4),
+                  fontSize: 16,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
